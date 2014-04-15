155f5d3e92d9fca2cc8af9083d02e033c350a242
Danny Dyla <dyladan@gmail.com>
2014-04-15 13:04:43 -0400

    changed tabs to spaces

--- plugins/reload.py
+++ plugins/reload.py
@@ -5,15 +5,15 @@ from datetime import datetime
 
 
 class Plug(irc.plugins.PluginTemplate):
-	"""Reloads plugins"""
-	def __init__(self):
-		super(Plug, self).__init__()
-		self.command = "reload"
+    """Reloads plugins"""
+    def __init__(self):
+        super(Plug, self).__init__()
+        self.command = "reload"
 
-	def call(self, msg, con):
-		nick, channel, params = irc.util.parseprivmsg(msg, con.nick)
-		start = datetime.utcnow()
-		con.ldplugins(con.plugdir)
-		elapsed = datetime.utcnow() - start
-		con.privmsg(channel, "reloaded plugins in %s seconds" % elapsed.total_seconds())
-		
+    def call(self, msg, con):
+        nick, channel, params = irc.util.parseprivmsg(msg, con.nick)
+        start = datetime.utcnow()
+        con.ldplugins(con.plugdir)
+        elapsed = datetime.utcnow() - start
+        con.privmsg(channel, "reloaded plugins in %s seconds" % elapsed.total_seconds())
+        
