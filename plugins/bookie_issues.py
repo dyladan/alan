cf5e7d09591d9b1c48702d55db93a793b938063a
Danny Dyla <dyladan@gmail.com>
2014-04-15 18:41:38 -0400

    fixed requirements.txt | added bookie issues and url shortener

---
+++ plugins/bookie_issues.py
@@ -0,0 +1,30 @@
+import re
+import irc.util
+import irc.plugins
+import requests
+import re
+
+class Plug(irc.plugins.PluginTemplate):
+    """Find bookie pull issues"""
+
+    def call(self, msg, con):
+        nick, channel, params = irc.util.parseprivmsg(msg, con.nick)
+
+        if not channel in ["#bookie", "#ubuntu-us-mi", "#asdf"]:
+            return
+
+        for arg in params:
+            match = re.match(r"#(\d+)$", arg)
+            if match:
+                issue = github_issue(match.group(1))
+                out = " - ".join([issue['state'], issue['title'], issue['html_url']])
+                if issue:
+                    con.privmsg(channel,out)
+
+def github_issue(issue):
+    r = requests.get("https://api.github.com/repos/bookieio/bookie/issues/%s" % issue)
+    if r.status_code == 200:
+        json = r.json()
+        return json
+    else:
+        return None
---
+++ plugins/url_shortener.py
@@ -0,0 +1,25 @@
+import re
+import irc.util
+import irc.plugins
+import urllib.parse
+import requests
+
+class Plug(irc.plugins.PluginTemplate):
+    """Shorten any urls"""
+    #def __init__(self):
+    #    super(Plug, self).__init__()
+
+    def call(self, msg, con):
+        nick, channel, params = irc.util.parseprivmsg(msg, con.nick)
+
+        message = " ".join(params)
+
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message)
+
+        for url in urls:
+            encoded = urllib.parse.quote(url)
+            request = "http://is.gd/create.php?format=json&url=%s" % encoded
+
+            isgd = requests.get(request).json()
+
+            con.privmsg(channel, isgd["shorturl"])
