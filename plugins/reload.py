976f3d36a47fe5fd1bbed0d0b8c34c12bfc02064
Danny Dyla <dyladan@gmail.com>
2014-04-15 17:18:50 -0400

    added help text

--- plugins/reload.py
+++ plugins/reload.py
@@ -9,6 +9,7 @@ class Plug(irc.plugins.PluginTemplate):
     def __init__(self):
         super(Plug, self).__init__()
         self.command = "reload"
+        self.helptext = "reloads plugins"
 
     def call(self, msg, con):
         nick, channel, params = irc.util.parseprivmsg(msg, con.nick)
--- plugins/uptime.py
+++ plugins/uptime.py
@@ -6,6 +6,7 @@ class Plug(irc.plugins.PluginTemplate):
 	def __init__(self):
 		super(Plug, self).__init__()
 		self.command = "uptime"
+		self.helptext = "returns uptime of current instance of this bot"
 
 	def call(self, msg, con):
 		nick, channel, params = irc.util.parseprivmsg(msg, con.nick)
