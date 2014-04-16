383cb46b7fed83f3d047e70ae874423cd88751f0
Danny Dyla <dyladan@gmail.com>
2014-04-15 20:00:00 -0400

    added source plugin

---
+++ plugins/source.py
@@ -0,0 +1,12 @@
+import irc.util
+import irc.plugins
+class Plug(irc.plugins.PluginTemplate):
+	def __init__(self):
+		super(Plug, self).__init__()
+		self.command = "source"
+		self.helptext = "links to current source code"
+
+	def call(self, ircmessage, con):
+	    nick, channel, params = irc.util.parseprivmsg(ircmessage, con.nick)
+	    
+	    con.privmsg(channel, "https://github.com/dyladan/alan")
