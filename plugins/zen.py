f4e135109d612cce087f30c18ab5a0b188becd19
Danny Dyla <dyladan@gmail.com>
2014-04-25 15:31:17 -0400

    zen

---
+++ plugins/zen.py
@@ -0,0 +1,43 @@
+import irc.util
+import irc.plugins
+
+class Plug(irc.plugins.PluginTemplate):
+    """Describe your plugin here"""
+    def __init__(self):
+        super(Plug, self).__init__()
+        self.command = "zen"
+        self.helptext = "The Zen of Python, by Tim Peters"
+        self.event = "PRIVMSG"
+        self.thread = True
+        self.private = False
+        self.name = None
+        self.protected = False
+
+        self.zen = list(map(str, """Beautiful is better than ugly.
+            Explicit is better than implicit.
+            Simple is better than complex.
+            Complex is better than complicated.
+            Flat is better than nested.
+            Sparse is better than dense.
+            Readability counts.
+            Special cases aren't special enough to break the rules.
+            Although practicality beats purity.
+            Errors should never pass silently.
+            Unless explicitly silenced.
+            In the face of ambiguity, refuse the temptation to guess.
+            There should be one-- and preferably only one --obvious way to do it.
+            Although that way may not be obvious at first unless you're Dutch.
+            Now is better than never.
+            Although never is often better than *right* now.
+            If the implementation is hard to explain, it's a bad idea.
+            If the implementation is easy to explain, it may be a good idea.
+            Namespaces are one honking great idea -- let's do more of those!""".splitlines()))
+
+    def call(self, ircmessage, con):
+        nick, channel, params = irc.util.parseprivmsg(ircmessage, con.nick)
+
+        con.privmsg(channel, self.zen[int(params[1])-1])
+        pass
+
+def str(line):
+    return line.strip()
